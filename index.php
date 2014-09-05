1488f85126342af49318e5bdff188d17b3507390
Carlosbarrera585c <carlosbarrera585@hotmail.com>
2014-09-05 11:33:15 -0500

    TALLER-1-CARLOS-BARRERA
    PRIMER TALLER PARA JULIAN LASSO

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description"content="practica de estilos"><!--es la descripcion general o global de su sitio web-->
+        <meta name="keywords"content="html5,css3,javascript"><!--son las palabras claves que necesito para trabajar en mi sitio web-->
+        <title>TAREA</title>
+        <link rel="stylesheet" href="css/miestilo.css"><!--para relacionar paginas web-->
+    </head>
+
+    <style type="text/css">
+
+        body {
+            background-color: #00BFFF;
+        }
+    </style>
+</head>
+
+<body>
+<center>
+    <div id="baseglobal">
+        <header id="encabezado">
+            <h1>TAREA DE PHP</h1>
+        </header>
+
+        <nav id="barramenu">
+            <ul><center>
+                    <li> <a href="Lista/index.php"> <input type="button" value="LISTA" style='background:#00BFFF; width:150px; height:50px'></a></li><br>
+                    <br><li> <a href="Insertar/index.php"><input type="button" value="INSERTAR" style='background:#00BFFF;width:150px; height:50px'> </a></li><br>
+                    <br><li> <a href="Modificar/index.php"><input type="button" value="MODIFICAR" style='background:#00BFFF;width:150px; height:50px'> </a></li><br>
+                    <br><li> <a href="Eliminar/index.php"><input type="button" value="ELIMINAR" style='background:#00BFFF;width:150px; height:50px'> </a></li><br>
+                    <br><li> <a href="Consultas/index.php"><input type="button" value="CONSULTAR" style='background:#00BFFF;width:150px; height:50px'> </a></li><br>
+                </center>
+            </ul>
+        </nav>
+</center>
+</body>
+</html>
